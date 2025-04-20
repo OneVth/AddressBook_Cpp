58a1c4a38a81bfba36b2305ba7161b34cfd3bcd2
OneVth <onebrotravel@gmail.com>
2025-04-20 19:53:45 +0900

    test: Add unit tests for validation functions (IsAllDigit, IsAllAlpha, IsPhoneFormat)

--- AddressBook_Test/test.cpp
+++ AddressBook_Test/test.cpp
@@ -4,7 +4,24 @@
 
 TEST(ValidatorTest, TestIsAllDigit)
 {
-	EXPECT_TRUE(Validator::IsAllDigit("12345"));
-	EXPECT_FALSE(Validator::IsAllDigit("123a5"));
-	EXPECT_FALSE(Validator::IsAllDigit(""));
+	EXPECT_TRUE(Validator::IsAllDigit("12345"));	// Pass
+	EXPECT_FALSE(Validator::IsAllDigit("123a5"));	// Fail	
+	EXPECT_FALSE(Validator::IsAllDigit("1@345"));	// Fail	
+	EXPECT_FALSE(Validator::IsAllDigit(""));		// Fail
+}
+
+TEST(ValidatorTest, TestIsAllAlpha)
+{
+	EXPECT_TRUE(Validator::IsAllAlpha("abcDEF"));	// Pass
+	EXPECT_FALSE(Validator::IsAllAlpha("abc123"));	// Fail
+	EXPECT_FALSE(Validator::IsAllAlpha("abc DEF"));	// Fail
+	EXPECT_FALSE(Validator::IsAllAlpha(""));	// Fail
+}
+
+TEST(ValidatorTest, TestIsPhoneFormat)
+{
+	EXPECT_TRUE(Validator::IsPhoneFormat("000-0000-0000"));		// Pass
+	EXPECT_FALSE(Validator::IsPhoneFormat("0000-0000-0000"));	// Fail
+	EXPECT_FALSE(Validator::IsPhoneFormat("000-aaaa-0000"));	// Fail
+	EXPECT_FALSE(Validator::IsPhoneFormat(""));	// Fail
 }
