8dacdc5a69ab74ccf7e252592c56d79f46c0491d
OneVth <onebrotravel@gmail.com>
2025-04-20 23:19:07 +0900

    feat: Implement basic ContactStore class with default constructor and destructor

--- AddressBook_Cpp/include/contact_store.h
+++ AddressBook_Cpp/include/contact_store.h
@@ -1,2 +1,16 @@
 #pragma once
 
+#include <iostream>
+#include <map>
+#include "contact.h"
+
+
+class ContactStore
+{
+private:
+	std::map<std::string, Contact> contacts;
+
+public:
+	ContactStore() = default;
+	~ContactStore() = default;
+};
