e4b29972509093d6aaf22ab14df9db05e5104868
OneVth <onebrotravel@gmail.com>
2025-04-28 00:13:55 +0900

    fix: Resolve merge conflict after git stash pop
    - Resolved conflicts in `main.cpp` and `ui_manager.cpp` after applying
      stashed changes
    - Adjusted conflicting code to ensure proper functionality and
      integration with the current branch
    - Completed conflict resolution and committed the merge changes

--- AddressBook_Cpp/main.cpp
+++ AddressBook_Cpp/main.cpp
@@ -4,107 +4,29 @@
 #include "contact.h"
 #include "contact_store.h"
 #include "file_manager.h"
+#include "ui_manager.h"
 
 int main(void)
 {
 	CreateDirectory(L".\\tests", NULL);
+<<<<<<< Updated upstream
 	
 	// Serialize a contact
 	ContactStore* store = new ContactStore();
+=======
 
-	Contact* c1 = new Contact(10, "Alice", "010-0000-1111");
-	Contact* c2 = new Contact(20, "Bob", "010-0000-2222");
-	Contact* c3 = new Contact(30, "Charlie", "010-0000-3333");
-	store->Insert(*c1);
-	store->Insert(*c2);
-	store->Insert(*c3);
+	std::string name;
+	std::string phone;
+	int age;
+>>>>>>> Stashed changes
 
-	if (FileManager::SaveToFile(L".\\tests\\test.dat", *store) == IO_SUCCESS)
+	while (UIManager::GetUserInfo(name, age, phone))
 	{
-		std::cout << "File saved successfully." << std::endl;
+		std::cout << "\nage: " << age << std::endl;
+		std::cout << "name: " << name << std::endl;
+		std::cout << "phone: " << phone << std::endl;
+		std::getchar(); // Wait for user input
 	}
-	else
-	{
-		std::cout << "Failed to save file." << std::endl;
-	}
-
-	delete c1;
-	delete c2;
-	delete c3;
-	delete store;
 	
-	ContactStore* searchStore = new ContactStore();
-	FileManager::LoadRecordFromFileByPhone(
-		L".\\tests\\test.dat",
-		"010-0000-1111",
-		*searchStore
-	);
-
-	searchStore->forEach([](const Contact& contact) {
-		std::cout << "Contact:\n" <<
-			"Age: " << contact.GetAge() << "\n"
-			<< "Name: " << contact.GetName() << "\n"
-			<< "Phone: " << contact.GetPhone() << std::endl;
-		}
-	);
-
-	// Deserialize the contact
-	/*ContactStore* loadedStore = new ContactStore();
-	char readBuffer[1024] = { 0 };
-
-	HANDLE hFileRead = CreateFile(
-		L".\\tests\\test.dat",
-		GENERIC_READ,
-		FILE_SHARE_READ,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL
-	);
-	if (hFileRead == INVALID_HANDLE_VALUE)
-	{
-		std::cout << "Failed to open file for reading." << std::endl;
-		return 1;
-	}
-
-	DWORD dwRead = 0;
-	BOOL bResult = FALSE;
-
-	bResult = ReadFile(hFileRead, readBuffer, 1024, &dwRead, NULL);
-	if (!bResult || dwRead == 0)
-	{
-		std::cout << "Failed to read from file." << std::endl;
-		CloseHandle(hFileRead);
-		return 1;
-	}
-
-	Contact* deserializedContact = new Contact();
-	int contactSize = deserializedContact->GetSize();
-	if(dwRead % contactSize != 0)
-	{
-		std::cout << "File size is not a multiple of contact size." << std::endl;
-		delete deserializedContact;
-		CloseHandle(hFileRead);
-		return 1;
-	}
-
-	for (int i = 0; i < ((int)dwRead / contactSize); i++)
-	{
-		deserializedContact->Deserialize(readBuffer + i * contactSize);
-		loadedStore->Insert(*deserializedContact);
-	}
-	std::cout << "Contact read from file successfully." << std::endl;
-
-	CloseHandle(hFileRead);
-
-	loadedStore->forEach([](const Contact& contact) {
-		std::cout << "Contact:\n" <<
-			"Age: " << contact.GetAge() << "\n"
-			<< "Name: " << contact.GetName() << "\n"
-			<< "Phone: " << contact.GetPhone() << std::endl;
-		});
-	
-	delete deserializedContact;
-	delete loadedStore;*/
 	return 0;
 }
