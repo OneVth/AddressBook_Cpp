a160aad0f55fadb316aa9402d6215bf38d35d933
OneVth <onebrotravel@gmail.com>
2025-04-28 00:18:39 +0900

    Revert "fix: Resolve merge conflict after git stash pop"
    This reverts commit e4b29972509093d6aaf22ab14df9db05e5104868.

--- AddressBook_Cpp/include/ui_manager.h
+++ AddressBook_Cpp/include/ui_manager.h
@@ -1,18 +1,1 @@
 #pragma once
-
-#include <iostream>
-
-class UIManager
-{
-private:
-
-public:
-	UIManager() = default;
-	~UIManager() = default;
-
-	static bool GetUserInfo(std::string& name, int& age, std::string& phone);
-
-	static bool GetUserAge(int& age);
-	static bool GetUserName(std::string& name);
-	static bool GetUserPhone(std::string& phone);
-};
